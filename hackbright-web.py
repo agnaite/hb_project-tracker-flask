c1d45c1f977f00d51d6efddb7b2b0c9eafa47a51
Agnė Klimaitė <agnaite@me.com>
2016-07-26 22:38:23 -0700

    added jinja partial

--- hackbright-web.py
+++ hackbright-web.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def get_student_form():
     """Show form for searching for a student."""
 
-    return render_template("student_search.html") 
+    return render_template("student_search.html")
 
 @app.route("/student")
 def get_student():
@@ -35,11 +35,12 @@ def student_add():
     github = request.form.get('github')
     firstname = request.form.get('first_name')
     lastname = request.form.get('last_name')
-    hackbright.make_new_student(firstname, lastname, github)     
+    hackbright.make_new_student(firstname, lastname, github)
     return render_template('student_info.html',
                             github=github, 
                             first=firstname,
                             last=lastname)
+
 
 @app.route('/project', methods=['GET'])
 def get_project():
@@ -51,7 +52,7 @@ def get_project():
     title = request.args.get('title')
     title, description, max_grade = hackbright.get_project_by_title(title)
     student_list = hackbright.get_grades_by_title(title)
-    
+
     for student in student_list:
         first_name, last_name, github = hackbright.get_student_by_github(student[0])
         student_names.append(first_name + " " + last_name)
@@ -67,12 +68,12 @@ def get_project():
                             student_list=student_list)
 
 
-
 @app.route('/project_search')
 def get_project_form():
     """Show form for searching for a project."""
 
-    return render_template("project_search.html") 
+    return render_template("project_search.html")
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
